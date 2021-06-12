b85636dde1e13632d916cfe46ddfc28fc6f43734
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-06-12 00:09:56 -0300

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,11 @@ TOKEN=$INPUT_TOKEN
 
 JSON_URL=https://api.github.com/repos/$OWNER/$REPO/releases/latest
 echo $JSON_URL
-/usr/bin/curl -u $TOKEN:x-oauth-basic $JSON_URL  > latest.json
-ID=`cat latest.json | jq '.assets[0].id' |  tr -d '"'`
+/usr/bin/curl -u $TOKEN:x-oauth-basic $JSON_URL > latest.json
+ID=`cat latest.json | jq '.assets[0].id' | tr -d '"'`
 echo "ID --> " $ID
 URL=https://$TOKEN@api.github.com/repos/$OWNER/$REPO/releases/assets/$ID
-curl -L -H "Accept:application/octet-stream" $URL | tar -xzv --strip-components=1
+/usr/bin/curl -L -H "Accept:application/octet-stream" $URL | tar -xzv --strip-components=1
 rm -rf latest.json
 
 #TOKEN=$INPUT_TOKEN

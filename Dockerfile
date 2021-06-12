8f3b7ea321f7e06c0be5c831b2c34b6498021185
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-06-12 00:27:58 -0300

    Test

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ ENV GITHUB_HEAD_REF = ""
 COPY entrypoint.sh /entrypoint.sh
 
 RUN apk add --update --no-cache bash curl
+RUN apk add --no-cache jq httpie
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ TOKEN=$INPUT_TOKEN
 JSON_URL=https://api.github.com/repos/$OWNER/$REPO/releases/latest
 echo $JSON_URL
 /usr/bin/curl -u $TOKEN:x-oauth-basic $JSON_URL > latest.json
-/usr/bin/sudo ID=`cat latest.json | jq '.assets[0].id' | tr -d '"'`
+ID=`cat latest.json | jq '.assets[0].id' | tr -d '"'`
 echo "ID --> " $ID
 URL=https://$TOKEN@api.github.com/repos/$OWNER/$REPO/releases/assets/$ID
 /usr/bin/curl -L -H "Accept:application/octet-stream" $URL | tar -xzv --strip-components=1

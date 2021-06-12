a05f07749717e5c20e1fef361ef099342361a039
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-06-12 16:28:35 -0300

    Teste

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,77 +1,32 @@
 #!/bin/sh -l
 
-OWNER="wpcodefactory"
-REPO="test-plugin"
+# Token 
 TOKEN=$INPUT_GITHUB_TOKEN
-#TAG=$INPUT_TAG_VERSION
+
+# File Param
 FILE_PARAM=$INPUT_FILE_PARAM
 
+# URL that will receive the file
+TO_URL=$INPUT_TO_URL
 
+# Tag version
 TAG="${GITHUB_REF:10}"
-if ! [[ -z ${INPUT_REPO} ]]; then
+if ! [[ -z ${$INPUT_TAG_VERSION} ]]; then
   TAG=$INPUT_TAG_VERSION
 fi
 
+# Repository, including author/repository
+REPO=$GITHUB_REPOSITORY
+if ! [[ -z ${INPUT_REPOSITORY} ]]; then
+  REPO=INPUT_REPOSITORY
+fi
 
-#JSON_URL=https://api.github.com/repos/$OWNER/$REPO/releases/latest
-#echo $JSON_URL
-#/usr/bin/curl -u $TOKEN:x-oauth-basic $JSON_URL > latest.json
-#cat latest.json
-#echo '!!'
-#ID=`cat latest.json | jq '.assets[0].id' | tr -d '"'`
-#echo "ID --> " $ID
-#URL=https://$TOKEN@api.github.com/repos/$OWNER/$REPO/releases/assets/$ID
-#/usr/bin/curl -L -H "Accept:application/octet-stream" $URL | tar -xzv --strip-components=1
-#rm -rf latest.json
+# Downloads the tag
+$(eval "curl -vLJO -H 'Authorization: token $TOKEN' 'https://github.com/$REPO/archive/refs/tags/$TAG.zip'")
 
-echo "TAG" + $TAG
-echo "FILE_PARAM" + $FILE_PARAM
-echo "GITHUB_REF" + $GITHUB_REF
+# Uploads the file
+RESPONSE=$(eval "curl -F 'custom_param=1234' -F '$FILE_PARAM=@$TAG.zip' '$TO_URL'")
 
-
-TESTE=$(eval "curl -vLJO -H 'Authorization: token $TOKEN' 'https://github.com/$OWNER/$REPO/archive/refs/tags/$TAG.zip'")
-ls
-
-UPLOAD=$(eval "curl -F '$FILE_PARAM=1234' -F '$FILE_PARAM=@$TAG.zip' 'http://ca4198430e4d.ngrok.io/wpdev/'")
-echo $UPLOAD;
-
-
-
-
-#CURL="curl -H 'Authorization: token $TOKEN' \
-      #https://api.github.com/repos/$OWNER/$REPO/releases"; \
-#ASSET_ID=$(eval "$CURL/tags/$TAG" | jq .id); \
-
-
-#echo $LATEST;
-#echo $TESTE;
-
-
-
-#TESTE=$(eval "curl -H 'Authorization: token $TOKEN' \
-#     -H 'Accept:application/octet-stream' \
-#     -i $DOWNLOAD_URL")
-
-
-
-
-
-
-#DOWNLOAD_URL=$(eval "$CURL/tags/$TAG" | jq .zipball_url); \
-
-#echo $DOWNLOAD_URL;
-
-#TESTE=$(eval "curl -H 'Authorization: token $TOKEN' \
-#     -H 'Accept:application/octet-stream' \
-#     -i $DOWNLOAD_URL")
-
-#echo $TESTE
-
-
-
-
-#eval "$CURL/assets/$ASSET_ID -LJOH 'Accept: application/octet-stream' --output test.zip"
-
-
-#curl -vLJO -H 'Authorization: token $INPUT_TOKEN' 'https://api.github.com/repos/$OWNER/$REPO/releases/assets/$ASSET_ID'
-#ls
+# Response
+#echo $RESPONSE;
+echo "::set-output name=response::$RESPONSE"

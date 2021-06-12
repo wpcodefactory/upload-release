88d16bcee577d7c6104fb6636d78b7f2093c8678
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-06-12 00:45:01 -0300

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,11 +17,26 @@
 # Script to download asset file from tag release using GitHub API v3.
 # See: http://stackoverflow.com/a/35688093/55075    
 
+GITHUB_API_TOKEN=$INPUT_TOKEN
+CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+
+# Check dependencies.
+set -e
+type curl grep sed tr >&2
+xargs=$(which gxargs || which xargs)
+
+# Validate settings.
+[ -f ~/.secrets ] && source ~/.secrets
+[ "$GITHUB_API_TOKEN" ] || { echo "Error: Please define GITHUB_API_TOKEN variable." >&2; exit 1; }
+[ $# -ne 4 ] && { echo "Usage: $0 [owner] [repo] [tag] [name]"; exit 1; }
+[ "$TRACE" ] && set -x
+read owner repo tag name <<<$@
+
 # Define variables.
 GH_API="https://api.github.com"
 GH_REPO="$GH_API/repos/wpcodefactory/test-plugin"
 GH_TAGS="$GH_REPO/releases/tags/1.0.0"
-AUTH="Authorization: token $INPUT_TOKEN"
+AUTH="Authorization: token $GITHUB_API_TOKEN"
 WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
 CURL_ARGS="-LJO#"
 

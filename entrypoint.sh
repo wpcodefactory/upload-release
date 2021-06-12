1c0279d1197df2830c392375f0d815db88c9e1d9
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-06-11 22:30:16 -0300

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ TOKEN=$1
 
 JSON_URL=https://api.github.com/repos/$OWNER/$REPO/releases/latest
 echo $JSON_URL
-curl -u $TOKEN:x-oauth-basic $JSON_URL  > latest.json
+sh "curl -u $TOKEN:x-oauth-basic $JSON_URL  > latest.json"
 ID=`cat latest.json | jq '.assets[0].id' |  tr -d '"'`
 echo "ID --> " $ID
 URL=https://$TOKEN@api.github.com/repos/$OWNER/$REPO/releases/assets/$ID

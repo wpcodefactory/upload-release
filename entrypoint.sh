a1e21c1f84090dee8c524b0ac8de06c6101bcd1b
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-06-12 17:30:37 -0300

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,15 @@
 # Token 
 TOKEN=$INPUT_GITHUB_TOKEN
 
-# Token 
+# URL Params 
 URL_PARAMS=$INPUT_URL_PARAMS
+CURL_URL_PARAMS=""
+if ! [[ -z ${$INPUT_URL_PARAMS} ]]; then
+  CURL_URL_PARAMS="-d $INPUT_FILE_PARAM"
+fi
 
 # File Param
 FILE_PARAM=$INPUT_FILE_PARAM
-CURL_FILE_PARAM=""
-if ! [[ -z ${$INPUT_FILE_PARAM} ]]; then
-  CURL_FILE_PARAM="-d $INPUT_FILE_PARAM"
-fi
 
 # URL that will receive the file
 TO_URL=$INPUT_TO_URL
@@ -34,7 +34,7 @@ echo $URL_PARAMS;
 $(eval "curl -vLJO -H 'Authorization: token $TOKEN' 'https://github.com/$REPO/archive/refs/tags/$TAG.zip'")
 
 # Uploads the file
-RESPONSE=$(eval "curl $CURL_FILE_PARAM -F 'custom_param=1234' -F '$FILE_PARAM=@$TAG.zip' '$TO_URL'")
+RESPONSE=$(eval "curl $CURL_URL_PARAMS -F 'custom_param=1234' -F '$FILE_PARAM=@$TAG.zip' '$TO_URL'")
 
 # Response
 #echo $RESPONSE;

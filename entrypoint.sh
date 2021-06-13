b139f71c2672d807bf1994ccb0e16de364bc7ccf
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-06-13 11:16:57 -0300

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,18 @@
 # Token 
 TOKEN=$INPUT_GITHUB_TOKEN
 
+echo '=====';
 # URL Params 
 URL_PARAMS=$INPUT_URL_PARAMS
-#CURL_URL_PARAMS=""
-#if ! [[ -z "$INPUT_URL_PARAMS" ]]; then
-#	jq -c '.[]' URL_PARAMS | while read i; do
-#	    # do stuff with $i
-#	done
-#fi
+CURL_URL_PARAMS=""
+if ! [[ -z "$INPUT_URL_PARAMS" ]]; then
+	for k in $(jq '.children.values | keys | .[]' <<< "$URL_PARAMS"); do
+		echo k;
+		value=$(jq -r ".children.values[$k]" file);
+		echo value
+	    #CURL_URL_PARAMS+= " -F ''"
+	done
+fi
 
 # File Param when sending to url
 FILE_PARAM=$INPUT_FILE_PARAM
@@ -30,18 +34,18 @@ if ! [[ -z ${INPUT_REPOSITORY} ]]; then
   REPO=$INPUT_REPOSITORY
 fi
 
-# Output filename
-OUTPUT_FILENAME="${REPO##*/}-$TAG.zip"
-if ! [[ -z ${INPUT_OUTPUT_FILENAME} ]]; then
-  OUTPUT_FILENAME=$INPUT_OUTPUT_FILENAME
+# Filename
+FILENAME="${REPO##*/}-$TAG.zip"
+if ! [[ -z ${INPUT_FILENAME} ]]; then
+  FILENAME=$INPUT_FILENAME
 fi
 
 # Downloads the tag
-GITHUB_RESPONSE=$(eval "curl -vLJ -H 'Authorization: token $TOKEN' 'https://api.github.com/repos/$REPO/zipball/$TAG' --output '$OUTPUT_FILENAME'")
+GITHUB_RESPONSE=$(eval "curl -vLJ -H 'Authorization: token $TOKEN' 'https://api.github.com/repos/$REPO/zipball/$TAG' --output '$FILENAME'")
 echo $GITHUB_RESPONSE;
 
 # Uploads the file
-RESPONSE=$(eval "curl -F 'custom_param=1234' -F '$FILE_PARAM=@$OUTPUT_FILENAME' '$TO_URL'")
+RESPONSE=$(eval "curl -F 'custom_param=1234' -F '$FILE_PARAM=@$FILENAME' '$TO_URL'")
 
 # Response
 echo "::set-output name=response::$RESPONSE"

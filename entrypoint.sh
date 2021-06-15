98d5fe82cd8da7cfd447110fdc75d5bbd3c5cf35
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-06-15 20:56:13 -0300

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,29 +3,16 @@
 # Token 
 TOKEN=$INPUT_GITHUB_TOKEN
 
-#echo '=====';
-# URL Params 
+# URL Params (https://www.reddit.com/r/bash/comments/97mvvh/iterating_through_json_kv_pairs/)
 URL_PARAMS=$INPUT_URL_PARAMS
-echo $URL_PARAMS
 CURL_URL_PARAMS=""
 if ! [[ -z "$INPUT_URL_PARAMS" ]]; then
 	shopt -s lastpipe
 	jq -r 'to_entries | map(.key + "|" + (.value | tostring)) | .[]' <<<"$URL_PARAMS" | \
-  	while IFS='|' read key value; do
-  		#echo "$key has value $value"
-  		#echo "$key=$value"
+  	while IFS='|' read key value; do  		
     	CURL_URL_PARAMS="$CURL_URL_PARAMS -F '$key=$value'"    	
-  	done
-	#jq -c '.[]' <<< "$URL_PARAMS" | while read i; do
-    #	echo $i;
-	#done
-	#for k in $(jq '.children.values | keys | .[]' <<< "$URL_PARAMS"); do		
-	#    #CURL_URL_PARAMS+= " -F ''"
-	#done | column -t -s$'\t'
+  	done	
 fi
-
-#echo "TEST:"
-echo $CURL_URL_PARAMS
 
 # File Param when sending to url
 FILE_PARAM=$INPUT_FILE_PARAM
@@ -55,8 +42,11 @@ fi
 GITHUB_RESPONSE=$(eval "curl -vLJ -H 'Authorization: token $TOKEN' 'https://api.github.com/repos/$REPO/zipball/$TAG' --output '$FILENAME'")
 echo $GITHUB_RESPONSE;
 
+unzip $FILENAME
+ls
+
 # Uploads the file
-RESPONSE=$(eval "curl $CURL_URL_PARAMS -F '$FILE_PARAM=@$FILENAME' '$TO_URL'")
+# RESPONSE=$(eval "curl $CURL_URL_PARAMS -F '$FILE_PARAM=@$FILENAME' '$TO_URL'")
 
 # Response
-echo "::set-output name=response::$RESPONSE"
+# echo "::set-output name=response::$RESPONSE"
